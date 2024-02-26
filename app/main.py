949e2a891ee609d89f9cfa15be1b8d732c0b07dc
Lu_cc <luca.var@gmail.com>
2024-02-26 15:39:14 +0100

    docker container created

--- app/main.py
+++ app/main.py
@@ -1,1 +1,18 @@
+# Import modules
+from fastapi import FastAPI
+from typing import Union
+
+app = FastAPI()
+
+
+@app.get("/home")
+def predict():
+    return {"prediction": "float", "status_code": "int"}
+
+
+@app.post("/predict")
+def predict():
+    return {"prediction": "float", "status_code": "int"}
+
+
 print("hello")
